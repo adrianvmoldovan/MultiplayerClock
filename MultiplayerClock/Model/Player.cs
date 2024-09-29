2f7607c1f6aeeebb356433224becd65e15807ac7
Adrian <adrianvmoldovan@yahoo.com>
2024-09-29 22:41:45 +0300

    Create main page and the corresponding view model

---
+++ MultiplayerClock/Model/Player.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace MultiplayerClock.Model
+{
+    public class Player /*: INotifyPropertyChanged*/
+    {
+        private string _name;
+        private Color _color;
+        private DateTime _time;
+
+        //public event PropertyChangedEventHandler PropertyChanged;
+
+        public Player()
+        {
+            _name = "";
+            _color = Colors.Black;
+            _time = DateTime.Now;
+        }
+        public Player(string name, Color color)
+        {
+            _name = name;
+            _color = color;
+            _time = DateTime.Now;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                //OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        public Color Color
+        {
+            get { return _color; }
+            set
+            {
+                _color = value;
+                //OnPropertyChanged(nameof(Color));
+            }
+        }
+
+        public DateTime Time
+        {
+            get { return _time; }
+            set
+            {
+                _time = value;
+                //OnPropertyChanged(nameof(Time));
+            }
+        }
+
+        //protected void OnPropertyChanged(string propertyName)
+        //{
+        //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        //}
+    }
+}
---
+++ MultiplayerClock/ViewModel/MainPageVM.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MultiplayerClock.Model;
+
+namespace MultiplayerClock.ViewModel
+{
+    public class MainPageVM : INotifyPropertyChanged
+    {
+        private List<Player> _players = new List<Player>
+        {
+            new Player("Adi", Colors.Blue),
+            new Player("Cami", Colors.Red),
+            new Player("Andi", Colors.Green),
+            new Player("Emi", Colors.Violet),
+        };
+        private GameType _selectedGameType;
+        private List<GameType> _availableGameTypes;
+        private bool _useSameTime;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public ObservableCollection<Player> Players { get; set; }
+
+        public MainPageVM(int numberOfPlayers)
+        {
+            Players = new ObservableCollection<Player>();
+            _players.ForEach(p => { if (Players.Count < numberOfPlayers) Players.Add(p); });
+
+            _selectedGameType = GameType.Classic;
+            _availableGameTypes = new List<GameType> { GameType.Classic, GameType.SuddenDeath };
+            _useSameTime = true;
+        }
+
+        public GameType SelectedGameType
+        {
+            get { return _selectedGameType; }
+            set
+            {
+                _selectedGameType = value;
+                OnPropertyChanged(nameof(SelectedGameType));
+            }
+        }
+
+        public List<GameType> AvailableGameTypes
+        {
+            get { return _availableGameTypes; }
+            set
+            {
+                _availableGameTypes = value;
+                OnPropertyChanged(nameof(AvailableGameTypes));
+            }
+        }
+
+        public bool UseSameTime
+        {
+            get { return _useSameTime; }
+            set
+            {
+                _useSameTime = value;
+                OnPropertyChanged(nameof(UseSameTime));
+            }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
