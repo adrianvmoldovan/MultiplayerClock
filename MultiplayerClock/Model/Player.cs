4a57728771fd15f7f6cd2d10f5f9b03c748e03ca
Adrian <adrianvmoldovan@yahoo.com>
2024-11-18 22:34:55 +0200

    Change TimePicketr to Stepper

--- MultiplayerClock/Model/Player.cs
+++ MultiplayerClock/Model/Player.cs
@@ -1,29 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using MultiplayerClock.Model.Colors;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
-using MultiplayerClock.Model.Colors;
+using Microsoft.Maui.Dispatching;
 
 namespace MultiplayerClock.Model
 {
     public class Player : INotifyPropertyChanged
     {
-        private string   _Name;
-        private string   _ColorName;
-        private Color    _Color;
-        private DateTime _Time;
+        private string           _Name;
+        private string           _ColorName;
+        private Color            _Color;
+        private int              _StartingMinutes;
+        private TimeSpan         _RemainingTime;
+        private IDispatcherTimer _Timer;
+        private string           _TimeLabel;
 
         public event PropertyChangedEventHandler? PropertyChanged;
 
-        public Player(string name, PossibleColor possibleColor)
+        public Player(string name, PossibleColor possibleColor, int minutes = 1)
         {
-            _Name      = name;
-            _Color     = possibleColor.Color;
-            _ColorName = possibleColor.Name;
-            _Time      = DateTime.Now;
+            _Name          = name;
+            _Color         = possibleColor.Color;
+            _ColorName     = possibleColor.Name;
+            _RemainingTime = TimeSpan.FromMinutes(minutes);
+            var dispatcher = Dispatcher.GetForCurrentThread();
+            if (dispatcher != null)
+            {
+                _Timer = dispatcher.CreateTimer();
+                _Timer.Interval = TimeSpan.FromSeconds(1);
+                _Timer.Tick += OnTimerTick;
+            }
+            else
+            {
+                throw new InvalidOperationException("Dispatcher is not available.");
+            }
+
+            _TimeLabel = _RemainingTime.ToString(@"mm\:ss");
+            _StartingMinutes = minutes;
+        }
+
+        private void OnTimerTick(object? sender, EventArgs e)
+        {
+            if (_RemainingTime.TotalSeconds > 0)
+            {
+                _RemainingTime = _RemainingTime.Subtract(TimeSpan.FromSeconds(1));
+                UpdateTimeLabel();
+            }
+            else
+            {
+                _Timer.Stop();
+            }
+        }
+
+        private void UpdateTimeLabel()
+        {
+            TimeLabel = _RemainingTime.ToString(@"mm\:ss");
         }
 
         public string Name
@@ -55,13 +85,23 @@ namespace MultiplayerClock.Model
             }
         }
 
-        public DateTime Time
+        public string TimeLabel
         {
-            get { return _Time; }
+            get { return _TimeLabel; }
             set
             {
-                _Time = value;
-                OnPropertyChanged(nameof(Time));
+                _TimeLabel = value;
+                OnPropertyChanged(nameof(TimeLabel));
+            }
+        }
+
+        public int StartingMinutes
+        {
+            get { return _StartingMinutes; }
+            set
+            {
+                _StartingMinutes = value;
+                OnPropertyChanged(nameof(StartingMinutes));
             }
         }
 
